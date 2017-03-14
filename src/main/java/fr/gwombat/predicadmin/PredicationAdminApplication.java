8d22f4b8756063627c3cdef092325c3f1cd973bd
Guillaume <guigui.fabbi@gmail.com>
2017-03-14 23:33:47 +0100

    Initial commit

---
+++ src/main/java/fr/gwombat/predicadmin/PredicationAdminApplication.java
@@ -0,0 +1,12 @@
+package fr.gwombat.predicadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredicationAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredicationAdminApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/gwombat/PredicationAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.gwombat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PredicationAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
