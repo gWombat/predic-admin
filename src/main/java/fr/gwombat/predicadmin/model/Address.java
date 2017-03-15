41a0335e64ff15a456529495e10840203ce5ecee
gWombat <guigui.fabbi@gmail.com>
2017-03-15 15:41:59 +0100

    Initial commit

---
+++ src/main/java/fr/gwombat/predicadmin/model/Address.java
@@ -0,0 +1,79 @@
+package fr.gwombat.predicadmin.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "addresses")
+public class Address extends AuditableEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "address_id")
+    private Long              id;
+
+    @NotBlank
+    @Length(max = 255)
+    @Column(name = "street_1", nullable = false, length = 255)
+    private String            street1;
+
+    @Length(max = 255)
+    @Column(name = "street_2", length = 255)
+    private String            street2;
+
+    @Column(name = "zip")
+    private String            zip;
+
+    @Column(name = "country")
+    private String            country;
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
