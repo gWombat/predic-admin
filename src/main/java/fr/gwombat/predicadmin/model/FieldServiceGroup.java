2981328ae8ee2dc47b01919bbcace55e858f190a
gWombat <guigui.fabbi@gmail.com>
2017-03-15 17:39:31 +0100

    Initial commit

---
+++ src/main/java/fr/gwombat/predicadmin/model/FieldServiceGroup.java
@@ -0,0 +1,43 @@
+package fr.gwombat.predicadmin.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "field_service_groups")
+public class FieldServiceGroup extends AuditableEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "group_id")
+    private Long              id;
+
+    @NotBlank
+    @Column(name = "name", nullable = false)
+    private String            name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
