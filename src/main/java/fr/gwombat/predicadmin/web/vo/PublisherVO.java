b3054973e9dac350cd618789c5aa9178bc660c6d
gWombat <guigui.fabbi@gmail.com>
2017-03-27 11:15:39 +0200

    Minor refactoring

--- src/main/java/fr/gwombat/predicadmin/web/vo/PublisherVO.java
+++ src/main/java/fr/gwombat/predicadmin/web/vo/PublisherVO.java
@@ -1,28 +1,39 @@
 package fr.gwombat.predicadmin.web.vo;
 
-import fr.gwombat.predicadmin.model.Publisher;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.util.Assert;
 
-import java.time.format.DateTimeFormatter;
+import fr.gwombat.predicadmin.model.Publisher;
 
 public class PublisherVO {
 
-    private final String fullName;
-    private final String birthDate;
-    private final String identifier;
+    private final String          fullName;
+    private final String          birthDate;
+    private final String          identifier;
+    private final String          baptismDate;
 
     private final ContactDetailVO contactDetail;
     private final AddressVO       address;
 
     public PublisherVO(final Publisher publisher) {
-        Assert.notNull(publisher, "");
-        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Assert.notNull(publisher, "the field [publisher] must not be null");
 
         this.fullName = publisher.getFullName();
-        this.birthDate = dateTimeFormatter.format(publisher.getBirthDate());
+        this.birthDate = formatDate(publisher.getBirthDate());
+        this.baptismDate = formatDate(publisher.getBaptismDate());
         this.identifier = publisher.getIdentifier();
         this.contactDetail = new ContactDetailVO(publisher.getContactDetail());
         this.address = new AddressVO(publisher.getAddress());
+    }
+
+    private static String formatDate(final LocalDate date) {
+        if (date != null) {
+            final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+            return dateTimeFormatter.format(date);
+        }
+        return "N/A";
     }
 
     public String getBirthDate() {
@@ -43,5 +54,9 @@ public class PublisherVO {
 
     public AddressVO getAddress() {
         return address;
+    }
+
+    public String getBaptismDate() {
+        return baptismDate;
     }
 }
