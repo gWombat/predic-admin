8290535f26310ed17dceb3299947463454d0796d
gWombat <guigui.fabbi@gmail.com>
2017-04-04 14:39:36 +0200

    Initial commit

---
+++ src/main/java/fr/gwombat/predicadmin/web/alert/DangerAlertMessage.java
@@ -0,0 +1,9 @@
+package fr.gwombat.predicadmin.web.alert;
+
+public class DangerAlertMessage extends AlertMessage {
+    
+    public DangerAlertMessage() {
+        super(AlertMessageLevel.DANGER);
+    }
+
+}
